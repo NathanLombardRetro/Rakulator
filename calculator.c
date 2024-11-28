3dc6501eb2dc1e602b1eba7e2706a9b1547a0050
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-11-28 11:32:52 +0200

    changed to c calculator

--- calculator.c
+++ calculator.c
@@ -1,33 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void clearScreen() {
+    system("cls || clear");
+}
+
+void displayMenu() {
+    printf("Simple Calculator\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
+    printf("4. Divide\n");
+    printf("5. Clear Screen\n");
+    printf("6. Exit\n");
+    printf("Choose an operation: ");
+}
 
 int main() {
-    char op;
-    double first, second;
+    int choice;
+    double num1, num2, result;
 
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &op);
+    while (1) {
+        clearScreen();
+        displayMenu();
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input. Please enter a valid option.\n");
+            while(getchar() != '\n');
+            continue;
+        }
 
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &first, &second);
+        if (choice == 6) {
+            break;
+        }
 
-    switch (op) {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf\n", first, second, first + second);
-            break;
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf\n", first, second, first - second);
-            break;
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf\n", first, second, first * second);
-            break;
-        case '/':
-            if (second != 0)
-                printf("%.1lf / %.1lf = %.1lf\n", first, second, first / second);
-            else
-                printf("Error! Division by zero.\n");
-            break;
-        default:
-            printf("Error! Invalid operator.\n");
+        if (choice == 5) {
+            clearScreen();
+            continue;
+        }
+
+        printf("Enter first number: ");
+        if (scanf("%lf", &num1) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            while(getchar() != '\n');
+            continue;
+        }
+
+        printf("Enter second number: ");
+        if (scanf("%lf", &num2) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            while(getchar() != '\n');
+            continue;
+        }
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 2:
+                result = num1 - num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 3:
+                result = num1 * num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    printf("Result: %.2lf\n", result);
+                } else {
+                    printf("Error: Division by zero!\n");
+                }
+                break;
+            default:
+                printf("Invalid choice! Please choose a valid operation.\n");
+                break;
+        }
+
+        printf("Press Enter to continue...");
+        while(getchar() != '\n');
+        getchar();
     }
 
     return 0;
